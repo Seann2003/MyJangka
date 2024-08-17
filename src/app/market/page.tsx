cea8261b304607161dacdf4f861c0e1a53f9f135
Junshen18 <junshenhere@gmail.com>
2024-08-18 05:07:06 +0800

    update leaderboard

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -36,66 +36,68 @@ export default function Home() {
   ];
 
   return (
-    <main className="h-screen w-[99%] flex flex-col items-center ">
-      <Navbar2 />
-      <div className="items-center flex flex-col">
-        <div className="flex flex-col gap-4 justify-center text-center w-[70%] mb-4">
-          <div className="relative">
-            <div className="flex justify-center">
-              <Carousel
-                opts={{
-                  align: "center",
-                  loop: true,
-                }}
-                className="w-[700px]"
-              >
-                <CarouselContent>
-                  {bannerData.map((data, index) => (
-                    <CarouselItem key={index}>
-                      <div className="bg-white h-auto w-full bg-fill flex justify-center items-center rounded-2xl">
-                        <Image
-                          src={data.image}
-                          alt={data.name}
-                          width={1080}
-                          height={400}
-                          className="object-contain rounded-2xl"
-                        />
-                      </div>
-                    </CarouselItem>
-                  ))}
-                </CarouselContent>
-
-                <CarouselPrevious variant={"link"} className="md:left-2" />
-                <CarouselNext variant={"link"} className="md:right-2" />
-              </Carousel>
-            </div>
-          </div>
-        </div>
-        <div className="flex-1 w-full">
-          <div className="flex border-b flex-row justify-between">
-            <div>
-              {tabs.map((tab) => (
-                <button
-                  key={tab}
-                  className={`px-4 py-2 ${
-                    activeTab === tab
-                      ? "transition-all border-b-2 border-blue-500 font-bold text-white"
-                      : "text-gray-400"
-                  }`}
-                  onClick={() => setActiveTab(tab)}
+    <>
+      <div className="h-screen w-[99%] flex flex-col items-center ">
+        <Navbar2 />
+        <div className="items-center flex flex-col">
+          <div className="flex flex-col gap-4 justify-center text-center w-[70%] mb-4">
+            <div className="relative">
+              <div className="flex justify-center">
+                <Carousel
+                  opts={{
+                    align: "center",
+                    loop: true,
+                  }}
+                  className="w-[700px]"
                 >
-                  {tab}
-                </button>
-              ))}
-            </div>
-            <SearchBar />
-          </div>
+                  <CarouselContent>
+                    {bannerData.map((data, index) => (
+                      <CarouselItem key={index}>
+                        <div className="bg-white h-auto w-full bg-fill flex justify-center items-center rounded-2xl">
+                          <Image
+                            src={data.image}
+                            alt={data.name}
+                            width={1080}
+                            height={400}
+                            className="object-contain rounded-2xl"
+                          />
+                        </div>
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
 
-          <div className="p-4">
-            <Marketplace />
+                  <CarouselPrevious variant={"link"} className="md:left-2" />
+                  <CarouselNext variant={"link"} className="md:right-2" />
+                </Carousel>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 w-full">
+            <div className="flex border-b flex-row justify-between">
+              <div>
+                {tabs.map((tab) => (
+                  <button
+                    key={tab}
+                    className={`px-4 py-2 ${
+                      activeTab === tab
+                        ? "transition-all border-b-2 border-blue-500 font-bold text-white"
+                        : "text-gray-400"
+                    }`}
+                    onClick={() => setActiveTab(tab)}
+                  >
+                    {tab}
+                  </button>
+                ))}
+              </div>
+              <SearchBar />
+            </div>
+
+            <div className="p-4">
+              <Marketplace />
+            </div>
           </div>
         </div>
       </div>
-    </main>
+    </>
   );
 }
